b55d40bc8ef8b0c5877079643787dc0c6c0342a8
YutongHan1123 <42613354+YutongHan1123@users.noreply.github.com>
2020-06-28 16:37:38 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function hideImage() {
-  let images = document.querySelectorAll('.img-car');
+let images = document.querySelectorAll('.img-car');
+array = [];
+for (let i = 0; i < images.length; i++) {
+  array.push(i);
+}
 
-  var i = Math.floor(Math.random() * 3);
-  console.log(i);
-  // images[i].style.display = 'none';
-  images[i].parentNode.style.display = 'none';
+function hideImage() {
+  if (array.length > 1) {
+    let deleted = Math.floor(Math.random() * array.length);
+    // images[deleted].parentNode.style.display = 'none';
+    // console.log(array);
+    // console.log(deleted);
+    deletedImage = array[deleted];
+    images[deletedImage].parentNode.parentNode.removeChild(images[deletedImage].parentNode);
+    array.splice(deleted, 1);
+    console.log(array);
+  }
+  else {
+    let deletedImage = array[0];
+    // console.log(deletedImage);
+    images[deletedImage].parentNode.parentNode.removeChild(images[deletedImage].parentNode);
+  }
 }
